c7934edf7622bf1d6d93d86f9681f9d052ae83d8
AndrewLondon <andrewmart@mail.com>
2024-01-21 11:32:03 +0000

    Updated

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -1,11 +1,15 @@
 package main
 
-import "database/sql"
+import (
+	"authentication/cmd/api/data"
+	"database/sql"
+)
 
 const webPort = "80"
 
 type Config struct {
-	DB *sql.DB
+	DB     *sql.DB
+	Models data.Models
 }
 
 func main() {
