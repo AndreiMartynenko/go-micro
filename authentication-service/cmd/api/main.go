c28d8cc56b0c76fed3ca9aa0c5bce7365edfb022
AndrewLondon <andrewmart@mail.com>
2024-01-21 16:58:45 +0000

    Postgres connection

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"authentication/cmd/api/data"
 	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 const webPort = "80"
@@ -13,5 +16,42 @@ type Config struct {
 }
 
 func main() {
+	log.Println("Starting authentication service")
+
+	// TODO connect to DB
+
+	// set up config
+	app := Config{}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Panic(err)
+
+	}
 
 }
+
+// Creating a connection to DB
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+// Now we are going to add Posgres to our Docker composed.yml file and we need to make
