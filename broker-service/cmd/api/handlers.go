56b36135aa24de21261f9783bc53e3fec16537c4
AndrewLondon <andrewmart@mail.com>
2024-01-22 16:11:33 +0000

    Postgres connection

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,8 +1,19 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 )
+
+type RequestPayload struct {
+	Action string      `json:"action"`
+	Auth   AuthPayload `json:"auth,omitempty"` // Will omit it if it's not there
+}
+
+type AuthPayload struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
@@ -19,5 +30,24 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
+	var requestPayload RequestPayload
+
+	err := app.readJSON(w, r, &requestPayload)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	switch requestPayload.Action {
+	case "auth":
+
+	default:
+		app.errorJSON(w, errors.New("unknown action"))
+
+	}
+
+}
+
+func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 
 }
