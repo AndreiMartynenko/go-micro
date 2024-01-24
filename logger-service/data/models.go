43c53c173b8e547f575bf1386172412e77306bbb
AndrewLondon <andrewmart@mail.com>
2024-01-24 17:30:58 +0000

    log-service

--- logger-service/data/models.go
+++ logger-service/data/models.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var client *mongo.Client
+
+func New(mongo *mongo.Client) Models {
+	client = mongo
+
+	return Models{
+		LogEntry: LogEntry{},
+	}
+}
+
+type Models struct {
+	LogEntry LogEntry
+}
+
+type LogEntry struct {
+	ID        string    `bson:"_id,omitempty" json:"id,omitempty"`
+	Name      string    `bson:"name" json:"name"`
+	Date      string    `bson:"date" json:"date"`
+	CreatedAt time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
+}
