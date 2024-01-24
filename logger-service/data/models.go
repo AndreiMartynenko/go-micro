9d7a3d6e5662e7e0af6f7d8cf87b71358954ca22
AndrewLondon <andrewmart@mail.com>
2024-01-24 17:34:45 +0000

    log-service

--- logger-service/data/models.go
+++ logger-service/data/models.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"log"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -23,7 +25,24 @@ type Models struct {
 type LogEntry struct {
 	ID        string    `bson:"_id,omitempty" json:"id,omitempty"`
 	Name      string    `bson:"name" json:"name"`
-	Date      string    `bson:"date" json:"date"`
+	Data      string    `bson:"data" json:"data"`
 	CreatedAt time.Time `bson:"created_at" json:"created_at"`
 	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
+}
+
+func (l *LogEntry) Insert(entry LogEntry) error {
+	collection := client.Database("logs").Collection("logs")
+
+	_, err := collection.InsertOne(context.TODO(), LogEntry{
+		Name:      entry.Name,
+		Data:      entry.Data,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	})
+	if err != nil {
+		log.Println("Error inserting into logs:", err)
+		return err
+	}
+
+	return nil
 }
