bf38d3b636a841261c8fe64d929bc6ad103bcb5a
AndrewLondon <andrewmart@mail.com>
2024-01-29 16:09:54 +0000

    Broker service updated

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -86,6 +86,27 @@ func (app *Config) sendMail(w http.ResponseWriter, msg MailPayload) {
 
 	request.Header.Set("Content-Type", "application/json")
 
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	//make sure we get back the right status code
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New("error calling mail service"))
+		return
+	}
+
+	//send back json
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "Message sent to " + msg.To
+
+	app.writeJSON(w, http.StatusAccepted, payload)
+
 }
 
 func (app *Config) logItem(w http.ResponseWriter, entry LogPayload) {
