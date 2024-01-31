61c4380984eb4f73d018e06282b0664ca5474e9c
AndrewLondon <andrewmart@mail.com>
2024-01-31 19:46:37 +0000

    event and consumer created

---
+++ listener-service/event/consumer.go
@@ -0,0 +1,25 @@
+package event
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Consumer struct {
+	conn      *amqp.Connection
+	queueName string
+}
+
+func NewConsumer(conn *amqp.Connection) (Consumer, error) {
+	consumer := Consumer{
+		conn: conn,
+	}
+
+}
+
+func (consumer *Consumer) setup() error {
+	channel, err := consumer.conn.Channel()
+	if err != nil {
+		return nil
+	}
+	return
+}
---
+++ listener-service/event/event.go
@@ -0,0 +1,3 @@
+package event
+
+amqp "github.com/rabbitmq/amqp091-go"
