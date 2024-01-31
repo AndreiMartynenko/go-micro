dcf1eec54afd62692dff140abcc9e6ecc9ef2bc0
AndrewLondon <andrewmart@mail.com>
2024-01-31 18:22:48 +0000

    RabbitMQ connection implemented

--- listener-service/main.go
+++ listener-service/main.go
@@ -1,10 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	//amqp - Advanced Messaging Queue Protocol
+	//This package amqp091-go replaces a community developed pack.
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
 func main() {
 
 	//try to connect to rabbitmq
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
 
-	//start listening for maessages
+	//start listening for messages
 	//What happens. This appli. is not going to periodically connect to the queue and listen
 	//for things that way
 	//Instead the queue will push it right to us
@@ -16,4 +34,37 @@ func main() {
 
 	// watch the queue and consume events
 
+}
+
+func connect() (*amqp.Connection, error) {
+	//this we're going to return once we successfully connect
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	//don't continue until rabbit is ready
+	for {
+		c, err := amqp.Dial("amqp://guest:geust@localhost") //login and password
+		if err != nil {
+			fmt.Println("RabbitMQ not yet ready...")
+			counts++
+		} else {
+			// if i connect break the loop
+			connection = c
+			break
+		}
+		// if we can't connect after five tries, something is wrong
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("backking off...")
+		time.Sleep(backOff)
+		continue
+
+	}
+
+	return connection, nil
 }
