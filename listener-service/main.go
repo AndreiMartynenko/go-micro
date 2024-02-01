7c48d266adc401061d447b13ad261efd549d4638
AndrewLondon <andrewmart@mail.com>
2024-02-01 16:04:18 +0000

    event and consumer created

--- listener-service/main.go
+++ listener-service/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"listenner/event"
 	"log"
 	"math"
 	"os"
@@ -19,10 +20,19 @@ func main() {
 
 	if err != nil {
 		log.Println(err)
+		//not executable
+		/*
+			os.Exit(1): This line immediately terminates the program and exits with a status code of 1.
+			In Unix-like operating systems, an exit status of 1 typically indicates that an error
+			occurred during program execution. By exiting the program with
+			a non-zero status code, it signals to the calling process (e.g., the shell)
+			that the program encountered an error.
+
+
+		*/
 		os.Exit(1)
 	}
 	defer rabbitConn.Close()
-	fmt.Println("Connected to RabbitMQ")
 
 	//start listening for messages
 	//What happens. This appli. is not going to periodically connect to the queue and listen
@@ -31,10 +41,20 @@ func main() {
 	//So we'll listen to certain queues
 	//And any time there's a event there, we actually get it directly from the queue
 
+	log.Println("Listening for and consuming RabbitMQ messages...")
+
 	// create consumer
 	// consumer consumes messages from the queue
+	consumer, err := event.NewConsumer(rabbitConn)
+	if err != nil {
+		panic(err)
+	}
 
 	// watch the queue and consume events
+	err = consumer.Listen([]string{"log.INFO", "log.WARNING", "log.ERROR"})
+	if err != nil {
+		log.Println(err)
+	}
 
 }
 
@@ -46,12 +66,14 @@ func connect() (*amqp.Connection, error) {
 
 	//don't continue until rabbit is ready
 	for {
-		c, err := amqp.Dial("amqp://guest:guest@localhost") //login and password
+		//c, err := amqp.Dial("amqp://guest:guest@localhost") //login and password
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq") //login and password
 		if err != nil {
 			fmt.Println("RabbitMQ not yet ready...")
 			counts++
 		} else {
-			// if i connect break the loop
+			// if I connect, break the loop
+			fmt.Println("Connected to RabbitMQ")
 			connection = c
 			break
 		}
@@ -60,7 +82,7 @@ func connect() (*amqp.Connection, error) {
 			fmt.Println(err)
 			return nil, err
 		}
-		// if we haven't tried at least five times or at most five times, then I'll back off that variable
+		// if we haven't tried at least five times or at most five times, then I'll back off
 		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
 		log.Println("backing off...")
 		time.Sleep(backOff)
