b3aede1e9a14e04568ee39f7e60e0deef64f4cde
AndrewLondon <andrewmart@mail.com>
2024-02-02 14:24:49 +0000

    broker service updated

---
+++ broker-service/event/emitter.go
@@ -0,0 +1,16 @@
+package event
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Emitter struct {
+	connection *amqp.Connection
+}
+
+func (e *Emitter) setup() error {
+	channel, err := e.connection.Channel()
+	if err != nil {
+		return err
+	}
+}
