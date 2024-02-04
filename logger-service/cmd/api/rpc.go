58f3f3147d7717a8f81b4904224744a769978f12
AndrewLondon <andrewmart@mail.com>
2024-02-04 16:16:22 +0000

    logger-service updated

--- logger-service/cmd/api/rpc.go
+++ logger-service/cmd/api/rpc.go
@@ -18,9 +18,11 @@ type RPCPayload struct {
 	Data string
 }
 
-// Then we declare, well, in our case, one function that has
+// Then we declare one function that has
 // the receiver pointer to our RPCServer.
 // And that requires a payload of some sort.
+// And then resp is a pointer to a string,
+// so we can send a message back to the people who call us.
 func (r *RPCServer) LogInfo(payload RPCPayload, resp *string) error {
 	collection := client.Database("logs").Collection("logs")
 	_, err := collection.InsertOne(context.TODO(), data.LogEntry{
@@ -28,6 +30,11 @@ func (r *RPCServer) LogInfo(payload RPCPayload, resp *string) error {
 		Data:      payload.Data,
 		CreatedAt: time.Now(),
 	})
+	/*
+	   So we process our data and we send the response back
+	   by setting the pointer to our *RESP,
+	   we give some kind of feedback there.
+	*/
 	if err != nil {
 		log.Println("error writing to mongo", err)
 		return err
